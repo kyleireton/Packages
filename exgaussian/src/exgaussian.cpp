6961734eb200945d7cad02846c2aa763e7261a06
kyleireton <kyle.ireton@gmail.com>
2023-04-02 01:02:21 +0000

    changing double to NumericVector in all cases

--- exgaussian/src/exgaussian.cpp
+++ exgaussian/src/exgaussian.cpp
@@ -5,18 +5,18 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 // Define the ex-Gaussian density function
-double d_exgaussian(double x, double mu, double sigma, double tau) {
-  double z = (x - mu) / sigma;
-  double lambda = 1.0 / tau;
-  double phi = R::dnorm(z, 0.0, 1.0, false);
-  double Phi = R::pnorm(lambda * z, 0.0, 1.0, true, false);
-  double c = lambda / 2.0 * exp(lambda * (mu + sigma * sigma / (2.0 * tau)) - lambda * x);
+NumericVector d_exgaussian(NumericVector x, NumericVector mu, NumericVector sigma, NumericVector tau) {
+  NumericVector z = (x - mu) / sigma;
+  NumericVector lambda = 1.0 / tau;
+  NumericVector phi = R::dnorm(z, 0.0, 1.0, false);
+  NumericVector Phi = R::pnorm(lambda * z, 0.0, 1.0, true, false);
+  NumericVector c = lambda / 2.0 * exp(lambda * (mu + sigma * sigma / (2.0 * tau)) - lambda * x);
   return c * Phi * exp(phi);
 }
 
 // Define the Rcpp wrapper function
 // [[Rcpp::export]]
-NumericVector exgaussian_cpp(double x, double mu, double sigma, double tau) {
+NumericVector exgaussian_cpp(NumericVector x, NumericVector mu, NumericVector sigma, NumericVector tau) {
   int n = x.length();
   NumericVector y(n);
   for (int i = 0; i < n; i++) {
--- exgaussian/src/exgaussian.h
+++ exgaussian/src/exgaussian.h
@@ -1,6 +1,6 @@
 #ifndef EXGAUSSIAN_H
 #define EXGAUSSIAN_H
 
-double d_exgaussian(double x, double mu, double sigma, double tau);
+NumericVector d_exgaussian(NumericVector x, NumericVector mu, NumericVector sigma, NumericVector tau);
 
 #endif // EXGAUSSIAN_H
